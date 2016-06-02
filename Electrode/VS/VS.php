f79f11ba7dc47e3b9668d48d552d4234cdfc39ac
Adama Dodo CISSE <adama.dodo.cisse@gmail.com>
2016-06-02 23:48:29 +0200

    Evolutions

--- Electrode/VS/VS.php
+++ Electrode/VS/VS.php
@@ -5,19 +5,35 @@ namespace Electrode\VS;
 use InvalidArgumentException;
 use RuntimeException;
 
-class VS
+/**
+ * Class VS
+ * @package Electrode\VS
+ */
+class VS implements \ArrayAccess
 {
-
-    private $parent;
-
-    private $filename;
-
+    /**
+     * @var
+     */
+    private $parent = null;
+    /**
+     * @var
+     */
+    private $filename  = null;
+    /**
+     * @var array
+     */
     private $parameters = array();
-
+    /**
+     * @var array
+     */
     private $cycles = array();
-
+    /**
+     * @var array
+     */
     private $sections = array();
-
+    /**
+     * @var array
+     */
     private $blocks = array();
 
     /**
@@ -136,5 +152,78 @@ class VS
     public function render()
     {
         echo $this->renderString();
+    }
+
+    /**
+     * @param $name
+     * @param null $default
+     * @return mixed|null
+     */
+    public function getParameter($name, $default = null)
+    {
+        return isset($this->parameters[$name]) ? $this->parameters[$name] : $default;
+    }
+
+    /**
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset <p>
+     * An offset to check for.
+     * </p>
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     * @since 5.0.0
+     */
+    public function offsetExists($offset)
+    {
+       return isset($this->parameters[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset <p>
+     * The offset to retrieve.
+     * </p>
+     * @return mixed Can return all value types.
+     * @since 5.0.0
+     */
+    public function offsetGet($offset)
+    {
+
+       return $this->getParameter($offset);
+    }
+
+    /**
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset <p>
+     * The offset to assign the value to.
+     * </p>
+     * @param mixed $value <p>
+     * The value to set.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->parameters[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset <p>
+     * The offset to unset.
+     * </p>
+     * @return void
+     * @since 5.0.0
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->parameters[$offset]);
     }
 }
